589f756967f421933134c820824da1a11233a1e2
jsncrz <jasoon.acruz@gmail.com>
2025-04-08 14:54:14 -0700

    Minor refactoring and changes to UI

--- src/features/characters/components/character-view.tsx
+++ src/features/characters/components/character-view.tsx
@@ -20,15 +20,15 @@ export const CharacterView = ({ characterId }: { characterId: string }) => {
   if (!character) return null;
 
   return (
-    <div className="bg-white/60 dark:bg-black/20 grid grid-flow-row grid-cols-2 gap-4 p-6 md:grid-cols-3">
+    <div className="border-2 border-black dark:border-white rounded-2xl grid grid-flow-row grid-cols-2 gap-4 p-6 md:grid-cols-3">
       <div className="size-full row-span-6 col-span-2 md:col-span-1">
         <img
-          className="border-2 border-foreground rounded-xl saturate-125 bg-background/80"
-          src={imgUrl + /*character.id*/ '43107' + '_full.png'}
+          className="m-auto max-w-full min-w-1/2 border-2 border-foreground rounded filter-(--profile-filters) lg:w-1/2"
+          src={imgUrl + character.id + '_profile.png'}
         ></img>
       </div>
       <div className="flex flex-col row-span-1 col-span-2 gap-6">
-        <div className="flex flex-col gap-4 bg-black/20 dark:bg-white/60 p-6 items-center md:flex-row border-b-2 justify-items-stretch">
+        <div className="flex flex-col gap-4 bg-black/20 dark:bg-white/60 p-6 items-center md:flex-row border-b-2 rounded justify-items-stretch">
           <div className="flex-2 flex gap-2 justify-self-start">
             <div className="flex-none content-center">
               <img
@@ -63,12 +63,12 @@ export const CharacterView = ({ characterId }: { characterId: string }) => {
             </div>
           </div>
         </div>
-        <div className="flex-1 text-center bg-black/20 dark:bg-white/60 p-6 border-y-2">
+        <div className="flex-1 text-center bg-black/20 dark:bg-white/60 p-6 border-y-2 rounded">
           <p className="text-lg italic text-white  dark:text-background">
             {character.acquisitionText}
           </p>
         </div>
-        <div className="flex-1 p-6 border-y-2">
+        <div className="flex-1 p-6 border-y-2 ">
           <p className="text-2xl italic text-center mb-4">PROFILE</p>
           <p className="text-lg">{character.description}</p>
         </div>
--- src/features/characters/components/characters-list.tsx
+++ src/features/characters/components/characters-list.tsx
@@ -3,11 +3,48 @@
 import { env } from '@/config/env';
 import { useCharacters } from '../api/get-characters';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Character } from '@/types/api';
 
 export type CharactersListProps = {};
 
+type CharacterEntryProps = {
+  imgUrl: string;
+  character: Character;
+  onClickEvent: () => void;
+  gradientColor: string;
+};
+
+const CharacterEntry = ({
+  imgUrl,
+  character,
+  onClickEvent,
+  gradientColor,
+}: CharacterEntryProps) => (
+  <div
+    onClick={onClickEvent}
+    key={character.id}
+    className={`h-full content-center cursor-pointer max-w-md rounded-3xl p-5px transition-[scale,box-shadow,border-width] duration-300 
+            ease-in-out hover:scale-105 hover:shadow-lg hover:border-4 border-3 border-foreground bg-gradient-to-b ${gradientColor}`}
+  >
+    <div className={`rounded-[calc(1.5rem-1px)] p-5 items-center text-center`}>
+      <img
+        className="size-full border-2 border-foreground rounded-xl"
+        src={imgUrl + character.id + '_icon.png'}
+      ></img>
+      <span className="text-md lg:text-lg font-bold antialiased text-shadow-md text-shadow-background/50">
+        {character.name}
+      </span>
+      <br />
+      <span className="text-sm lg:text-md antialiased text-shadow-sm text-shadow-background/50">
+        {character.anotherName}
+      </span>
+    </div>
+  </div>
+);
+
 export const CharactersList = ({}: CharactersListProps) => {
-  const router = useRouter()
+  const router = useRouter();
+  const imgUrl = env.IMAGE_URL;
   const searchParams = useSearchParams();
   const locale = searchParams?.get('locale')
     ? String(searchParams.get('locale'))
@@ -16,8 +53,6 @@ export const CharactersList = ({}: CharactersListProps) => {
   const charactersQuery = useCharacters({
     locale: locale,
   });
-  const imgUrl = env.IMAGE_URL;
-
   if (charactersQuery.isLoading) {
     return (
       <div className="flex h-48 w-full items-center justify-center">HELLO</div>
@@ -29,7 +64,7 @@ export const CharactersList = ({}: CharactersListProps) => {
   if (!characters) return null;
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-6 gap-8 items-center">
+    <div className="grid grid-cols-3 lg:grid-cols-6 gap-8 items-center">
       {characters.map((chara) => {
         const attributeVariants = {
           fire: 'from-fire-dark/25 to-fire-light/25 hover:shadow-fire-light/20',
@@ -38,30 +73,20 @@ export const CharactersList = ({}: CharactersListProps) => {
           air: 'from-air-dark/25 to-air-light/25 hover:shadow-air-light/20',
           strike:
             'from-strike-dark/25 to-strike-light/25 hover:shadow-strike-light/20',
-          slash: 'from-slash-dark/25 to-slash-light/25 hover:shadow-slash-light/20',
+          slash:
+            'from-slash-dark/25 to-slash-light/25 hover:shadow-slash-light/20',
           stab: 'from-stab-dark/25 to-stab-light/25 hover:shadow-stab-light/20',
         };
         const attribute =
           chara.attackAttribute.toLocaleLowerCase() as keyof typeof attributeVariants;
         return (
-          <div
-            onClick={() => router.push(`/characters/${chara.id}`)}
+          <CharacterEntry
             key={chara.id}
-            className={`cursor-pointer max-w-md rounded-3xl p-5px transition-[scale,box-shadow,border-width] duration-300 
-            ease-in-out hover:scale-105 hover:shadow-lg hover:border-4 border-3 border-foreground bg-gradient-to-b ${attributeVariants[attribute]}`}
-          >
-            <div
-              className={`rounded-[calc(1.5rem-1px)] p-5 items-center text-center`}
-            >
-              <img
-                className="size-full border-2 border-foreground rounded-xl"
-                src={imgUrl + chara.id + '_icon.png'}
-              ></img>
-              <span className="text-lg font-bold antialiased text-shadow-md text-shadow-background/50">{chara.name}</span>
-              <br />
-              <span className="text-md antialiased text-shadow-sm text-shadow-background/50">{chara.anotherName}</span>
-            </div>
-          </div>
+            character={chara}
+            gradientColor={attributeVariants[attribute]}
+            imgUrl={imgUrl}
+            onClickEvent={() => router.push(`/characters/${chara.id}`)}
+          ></CharacterEntry>
         );
       })}
     </div>
